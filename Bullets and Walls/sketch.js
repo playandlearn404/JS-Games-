ffe024583aac01124acca41ce1c6480032ada38c
playandlearn404 <devamritapython@gmail.com>
2020-07-18 14:53:06 +0530

    Updated

--- Bullets and Walls/sketch.js
+++ Bullets and Walls/sketch.js
@@ -3,23 +3,25 @@ var bullet, speed, thickness;
 
 function setup() {
   createCanvas(1800,800);
-  wall = createSprite(1200, 400, thickness, height / 2);
+  
   thickness = random(22, 83); 
   weight = random(30, 52);
   speed = random(223, 321);
   
   bullet = createSprite(100, 400, 20, 20);
-  bullet.velocityX = random(10, 35);
+  bullet.velocityX = random(223, 321);
+
+  wall = createSprite(1200, 400, thickness, height / 2);
 }
 
 function draw() {
   background(0,0,0);  
   if (hasCollided(bullet, wall)) {
     if (damageCalculator(speed, thickness, weight) > 10) {
-        bullet.shapeColor = "red"
+        wall.shapeColor = "red"
     }
     else{
-        bullet.shapeColor = "green"
+        wall.shapeColor = "green"
     }
   }
   drawSprites();
